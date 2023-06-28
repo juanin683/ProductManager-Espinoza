073672303c7d85bb397d6a00d9926dad41f438c8
juana espinoza <juana.espinoza543@gmail.com>
2023-06-28 15:26:46 -0300

    corregi update y delete

--- main.js
+++ main.js
@@ -34,11 +34,11 @@ class ProductManager {
         
         this.products = productsInFile;
 
-        // this.products.push({
-        //     id: this.#id++,
-        //     ...product,
-            
-        // });
+          this.products.push({
+             id: this.#id++,
+              ...product,
+      
+          });
 
         if (this.products.length === 0) {
             this.#id = 1;
@@ -64,7 +64,7 @@ class ProductManager {
     getProductById = async(id) => {
         
         let readFile = await fs.promises.readFile(this.path, "utf8")
-        this.products = (JSON.parse(readFile))
+        this.products = JSON.parse(readFile)
         
         const misProductos = this.products.find((prod) => prod.id === id)
         return !misProductos ? ("NOT FOUND") : misProductos;
@@ -73,28 +73,49 @@ class ProductManager {
 
     
     deleteProduct = async(id)=>{
-        let readFile3 = await fs.promises.readFile(this.path, "utf8")
-        return (JSON.parse(readFile3))
+        let readFile = await fs.promises.readFile(this.path, "utf8")
+        this.products =  JSON.parse(readFile)
         
-        let deleteProductId = readFile3.filter(prod => prod.id === id)
-        await fs.promises.writeFile(this.path, JSON.stringify(deleteProductId))
+        let deleteProductId = this.products.find((product) => product.id === id)
+        
+        if (!deleteProductId) {
+            return `No se ha encontrado ese producto con id ${id}`;
+        } 
+
+        let deleteProd = this.products.filter((p) => p.id !== id)
+        
+        await fs.promises.writeFile(this.path, JSON.stringify(deleteProd,null,2))
 
     }
-    
-    updateProduct = async({id, ...productManager})=>{
+
+    updateProduct = async(id, product)=>{
         
+        let readFile = await fs.promises.readFile(this.path, "utf8")
+        this.products= JSON.parse(readFile)
+
+       let productOld = this.products.find((product) => product.id === id)
+        
+       if (!productOld) {
+        return `No se ha encontrado ese producto con id ${id}`;
+        } 
+
+    let update = this.products.map((p) => {
+        if (p.id ===id) {
+            return {...p, ...product};
+        }
+        return p;
+    })
+    await fs.promises.writeFile(this.path, JSON.stringify(update,null,2))
     
-        await this.deleteProductId(id);
-       let productOld = await readFile
     }
+
 }
 
 const productManager = new ProductManager();
 
 
-
 let product1 = { 
-    " title": "Royal Canin Gastrointestinal LOW FAT DOG x 1.5kg",
+    "title": "Royal Canin Gastrointestinal LOW FAT DOG x 1.5kg",
     "description":"Edad: Adulto Tipo de Alimento:Seco Peso: 1.5kg",
     "price": 3680,
     "thumbnail": "https://hollywoodpetshop.com.ar/wp-content/uploads/2022/04/GASTROINTESTINAL-LOW-FAT-DOG-300x300.jpg",
@@ -102,34 +123,42 @@ let product1 = {
     "stock": 10,
     "categoria": "Perros"}
     
-await productManager.addProducts(product1)
 
-console.log(productManager.getProducts(1))
+let product2 = {
+    "title": "Cat Chow Adulto Peso Saludable x 3kg",
+    "description":  "Edad: Adulto Tipo de Alimento:Seco Peso:3 kg Reducido en calorias para ayudar a controlar su peso y fortalecer sus defensas",
+    "price": 3280,
+    "thumbnail": "https://hollywoodpetshop.com.ar/wp-content/uploads/2019/01/Cat-chow-peso-saludable-1.png",
+    "code": "Gatos1",
+    "stock": 15,
+    "categoria": "Gatos"}
+
+
+await productManager.addProducts(product1)
+await productManager.addProducts(product2)
+
+
 
 // let p = productManager.getProductById(1)
 // console.log(p)
+let p1 = await productManager.getProductById(0)
+console.log(p1)
 
-// productManager.deleteProductId(1)
+let p2 = await productManager.getProductById(1)
+console.log(p2)
 
-//1
-productManager.updateProduct({
-    " title": "Royal Canin Gastrointestinal LOW FAT DOG x 1.5kg",
-    "description":"Edad: Adulto Tipo de Alimento:Seco Peso: 1.5kg",
-     "price": 3900,
-     "thumbnail": "https://hollywoodpetshop.com.ar/wp-content/uploads/2022/04/GASTROINTESTINAL-LOW-FAT-DOG-300x300.jpg",
-     "code": "Perros1",
-     "stock": 10,
-     "categoria": "Perros"
-    
-    })
+let p1Update = await productManager.updateProduct({
+   " title": "Royal Canin Gastrointestinal LOW FAT DOG x 1.5kg",
+   "description":"Edad: Adulto Tipo de Alimento:Seco Peso: 1.5kg",
+   "price": 3900,
+   "thumbnail": "https://hollywoodpetshop.com.ar/wp-content/uploads/2022/04/GASTROINTESTINAL-LOW-FAT-DOG-300x300.jpg",
+   "code": "Perros1",
+   "stock": 20,
+   "categoria": "Perros"
+   })
+console.log(p1Update)
 
-    
-//2    
-    // {
-    //    " title": "Cat Chow Adulto Peso Saludable x 3kg",
-    //     "description":  "Edad: Adulto Tipo de Alimento:Seco Peso:3 kg Reducido en calorias para ayudar a controlar su peso y fortalecer sus defensas",
-    //     "price": 3280,
-    //     "thumbnail": "https://hollywoodpetshop.com.ar/wp-content/uploads/2019/01/Cat-chow-peso-saludable-1.png",
-    //     "code": "Gatos1",
-    //     "stock": 15,
-    //     "categoria": "Gatos"
+// let deleteo = productManager.deleteProduct(1)
+// console.log(deleteo)
+
+ console.log(productManager.getProducts(1))
