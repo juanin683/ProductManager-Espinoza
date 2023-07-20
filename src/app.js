f858823cfd2c259667d85ef37ee73361f4799f7c
juana espinoza <juana.espinoza543@gmail.com>
2023-07-20 01:05:16 -0300

    ultimo update

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import express from "express";
 import handlebars from "express-handlebars";
-import __dirname from "./config/multer.js"
 import { Server as SocketServer } from "socket.io";
 import ProductManager from "./ProductManager.js";
 import CartManager from "./CartManager.js";
 import ProductManagerRouter from "./routes/ProductManager.router.js";
 import ProductViewsRouter from "./routes/products.views.router.js";
 import Cart from "./routes/Cart.router.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
@@ -21,7 +22,7 @@ const appServer = app.listen(8080, () => {
 const socketio = new SocketServer(appServer)
 
 app.engine("handlebars", handlebars.engine())
-app.set('views',`src/views`)
+app.set('views',`${__dirname}/views`)
 app.set("view engine", 'handlebars')
 
 
@@ -39,10 +40,12 @@ app.use("/api/carts",Cart);
 
 
 
-app.get("/",(req,res)=>{
+app.get("/realtimeproducts",(req,res)=>{
 res.render("realTimeProducts")
 })
 
+
+const prodmanager = new ProductManager(`${__dirname}/src/db/products.json`)
 //products con websockets
 socketio.on("/realtimeproducts", async (req, res) => {
 
@@ -50,11 +53,26 @@ socketio.on("/realtimeproducts", async (req, res) => {
 });
 
 socketio.on("conection",async(socket)=>{
-    const productList = await ProductManager.getProducts();
-    socket.emit("products",productList)
+    const productList = await prodmanager.getProducts({});
+    socket.emit("sendAllProducts",productList)
     console.log("socket connection")
+
 })
 
+
+socketio.on("addProducts",async(p)=>{
+    await prodmanager.addProducts(p);
+    const updProducts = await prodmanager.getProducts({})
+    socket.emit("sendAllProducts",updProducts)
+    console.log("socket connection")
+
+})
+
+socketio.on("deleteProduct", async (id) => {
+    await prodmanager.deleteProduct(id);
+    const updProducts = await prodmanager.getProducts({});
+    socketio.emit("sendAllProducts", updProducts);
+  });
 
 
 
--- src/public/js/socket.js
+++ src/public/js/socket.js
@@ -2,6 +2,71 @@
 
 const socket = io();
 console.log("hola front")
-socket.on("products",(list)=>{
-    
+
+
+socket.on("sendAllProducts",(p)=>{
+    upProducts(p)
 })
+
+function upProducts(p){
+    const container = document.getElementById("list-products")
+    const prods = "";
+
+    p.forEach((e) => {
+        prods += `
+        <form action="" method="post">
+    <article id="list-products">
+    <div>
+        <img src="${e.thumbnail}" alt="">
+    <ul>
+
+        <li>
+            Titulo: ${e.title}
+        </li>
+        <li>
+            Descripción: ${e.description}
+        </li>
+        <li>
+            Precio: ${e.price}
+        </li>
+        <li>
+            Stock: ${e.stock}
+        </li>
+    </ul>
+    </div>
+    </article>
+
+    
+</form>`
+    });
+
+container.innerHTML = prods;
+}
+
+let form = document.getElementById("form-real-time-prods")
+form.addEventListener("add-btn",(event)=>{
+    event.preventDefault()
+   
+    socket.emit("addProducts",{
+        title,
+        description,
+        stock,
+        thumbnail,
+        categoria,
+        price,
+        code,
+
+    })
+})
+
+document.getElementById("delete-btn").addEventListener("click", function () {
+    const dltedinput = document.getElementById("id-prod-input");
+    const deleteid = parseInt(dltedinput.value);
+    socket.emit("deleteProduct", deleteid);
+    dltedinput.value = "";
+  });
+socket.on("upProds", (p) => {
+  upProducts(p);
+});
+
+
