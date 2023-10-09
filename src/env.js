93ea98d89695938f4961387f9f731a5133feeef2
juana espinoza <juana.espinoza543@gmail.com>
2023-10-09 18:59:17 -0300

    cambios para el desafio

--- src/env.js
+++ src/env.js
@@ -1,14 +1,23 @@
 import {Command} from "commander"
 import dotenv from "dotenv"
 
+
 const program = new Command();
 
 program.option('-p <port>', 'port to init app', 8080)
+program.option('--mode <mode>','mode of execution','development')
 
 program.parse();
 
 dotenv.config();
-
 export default {
     persistence: process.env.PERSISTENCE,
+    LINK_DB: process.env.MONGO_URI,
+    PORT: process.env.PORT || 8080,
+  
   };
+
+  dotenv.config({
+    path: options.mode=='production' ? './.env.production' :'./.env.development',
+});
+  
